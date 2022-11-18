0ccc7f485c3699f99f0752cb149dedd30f260649
Aniket Kumar <aniketkumar693@gmail.com>
2022-11-18 03:14:54 -0600

    frontend setup

---
+++ Frontend/stats-visualize-react/src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [data, setData] = React.useState(null);
+  let myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const raw = `
+query{
+  statistics{
+      state
+      total_cases
+      total_death
+      new_death
+      new_case
+      submission_date
+  }
+}`;
+
+
+  React.useEffect(() => {
+    fetch("/stats", {
+      method: 'post',
+      headers: {'Content-Type':'application/graphql'},
+      body: raw
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data.data.statistics[0].state)
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
